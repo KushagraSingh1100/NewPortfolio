6942b9d599c2970efbc5c4e696aaca600faf7421
KushagraSingh1100 <kushagrasingh1100@gmail.com>
2025-01-13 19:16:59 +0530

    server

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -6,16 +6,12 @@ const Message = require("./model/messageModel");
 
 const app = express();
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
   next();
 });
 app.use((req, res, next) => {
